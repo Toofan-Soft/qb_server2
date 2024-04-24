df6e043314d5081dbf0466158e27f7afb5b987d5
fadi-mofeed <102834190+fadi-mofeed@users.noreply.github.com>
2024-04-24 03:03:23 +0300

    write algorithm structure

---
+++ app/Algorithm/AlgorithmView.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Algorithm;
+
+class AlgorithmView{
+ 
+// $duration, $languageId, $difficultyLevelId, 
+// $formsCount, $formConfigurationMethodId, $questionsTypes, $topicsIds
+
+    public static function generateOnlineExam($request)
+    {
+
+    }
+
+    public static function generatePaperExam($request)
+    {
+
+    }
+
+
+    public static function updateStudentQuestionUsageAnswer($formId, $questionId, $answer){
+
+    }
+
+    public static function updateGuestQuestionUsageAnswer($examId, $questionId, $answer){
+
+    }
+
+}
